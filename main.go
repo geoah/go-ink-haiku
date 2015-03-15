a7991155b1aa7d32291eede3e3c7b75ba39960af
George Antoniadis <george@noodles.gr>
2015-03-15 22:59:15 +0200

    Add interpose and use a subrouter

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/carbocation/interpose"
 )
+
+// const ContextIdentity key = 0
 
 func main() {
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	middle := interpose.New()
+	middle.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// context.Set(req, ContextHostname, Get)
+		rw.Header().Set("X-Server-Name", "Interpose Test Server")
+	}))
+	middle.UseHandler(router)
+
+	log.Fatal(http.ListenAndServe(":8080", middle))
 }
--- router.go
+++ router.go
@@ -5,10 +5,11 @@ import "github.com/gorilla/mux"
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
-	// router.HandleFunc("/", Index).Name("Index")
-	router.HandleFunc("/nodes", NodeIndex).Name("NodeIndex")
-	// router.HandleFunc("/nodes", TodoCreate).Methods("POST").Name("TodoCreate")
-	router.HandleFunc("/nodes/{nodeId}", NodeShow).Name("TodoShow")
+	subrouter := router.Host("{fqdn:[a-z\\.]+}").Subrouter()
+	subrouter.HandleFunc("/", Index).Name("Index")
+	subrouter.HandleFunc("/nodes", NodeIndex).Name("NodeIndex")
+	// subrouter.HandleFunc("/nodes", TodoCreate).Methods("POST").Name("NodeCreate")
+	subrouter.HandleFunc("/nodes/{nodeId}", NodeShow).Name("NodeShow")
 
 	return router
 }
